a36b73e339e6ab44f0a1fadce9417082b7d1e678
Leah Boyd <leahboyd08@gmail.com>
2019-03-18 23:18:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -34,8 +34,19 @@ end
 def using_uniq(array)
   array.uniq { |duplicate_items| }.to_h
 end
-  
+
 def using_flatten(array)
   array.flatten
 end
+  
+def using_delete(array, string)
+  array.delete(string)
+end
 
+def using_delete_at(array, integer)
+  array.delete(2)
+end
+  
+  
+  
+  
